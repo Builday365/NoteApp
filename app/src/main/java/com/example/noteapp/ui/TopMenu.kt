c5e5a6906dca7ff4a0e033cb19054efd9bfbc928
yongyeon.yu <axony99@gmail.com>
2024-04-02 02:03:26 +0900

    Adjusted box color based on selectedDate.

--- app/src/main/java/com/example/noteapp/ui/TopMenu.kt
+++ app/src/main/java/com/example/noteapp/ui/TopMenu.kt
@@ -1,6 +1,5 @@
 package com.example.noteapp.ui
 
-import CalendarMenu
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
